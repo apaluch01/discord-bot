4689ca5ab805ed1b74a80e2d4e84527b7a5647d0
apaluch01 <ola.paluch88@gmail.com>
2023-09-19 21:49:25 +0200

    javacord added

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+package com.github.yourname;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "MTE1Mzc2NTkwODkwNTQ2Mzg4OQ.GTqPxX.UabBzP_BCHrcADGthFAWZMOC2Q7ElvY2uN0bHU";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
