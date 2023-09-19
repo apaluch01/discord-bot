a239fe4f3edc95bf56ceba83a36b5e9c134cd947
apaluch01 <ola.paluch88@gmail.com>
2023-09-19 22:53:44 +0200

    message try outs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,37 @@ package com.github.yourname;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Insert your bot's token here
-        String token = "MTE1Mzc2NTkwODkwNTQ2Mzg4OQ.GTqPxX.UabBzP_BCHrcADGthFAWZMOC2Q7ElvY2uN0bHU";
+
+        String token = null;
+
+        try (FileReader file = new FileReader("src/config.txt");
+             BufferedReader reader = new BufferedReader(file)) {
+             token = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
-        api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Pong!");
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction slashCommandInteraction = event.getSlashCommandInteraction();
+            if (slashCommandInteraction.getCommandName().equals("ping")) {
+                slashCommandInteraction.createImmediateResponder()
+                        .setContent("Pong!")
+                        .setFlags(MessageFlag.EPHEMERAL) // Only visible for the user which invoked the command
+                        .respond();
             }
-        });
 
         // Print the invite url of your bot
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
